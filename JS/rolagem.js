bfaa2876310458ffcada281b627e09392f7771d9
HelesT <nyltondudu29@gmail.com>
2023-07-12 18:39:57 -0300

    a

--- JS/rolagem.js
+++ JS/rolagem.js
@@ -2,7 +2,6 @@ var swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     slidesPerView: 1,
     spaceBetween: 0,
-    keyboard: true,
     on: {
       slideChange: function () {
         document.documentElement.scrollTop = 0; // Para navegadores modernos
