096a4a9bd3e42c25aab57623ee1095eed1f46e4b
HelesT <nyltondudu29@gmail.com>
2023-09-14 16:15:10 -0300

    S2 paciente php test

---
+++ JS/index.js
@@ -0,0 +1,48 @@
+function PacienteRegistro(){
+
+    var Data = $('#data_paciente').val();
+    var NomeHospitalPaciente = $('#nome_hospital_paciente').val();
+    var NomePaciente = $('#nome_paciente').val();
+    var IdadePaciente = $('#idade_paciente').val();
+    var RgCpfPaciente = $('#rg_cpf_paciente').val();
+    var FonePaciente = $('#fone_paciente').val();
+    var AcompanhantePaciente = $('#acompanhante_paciente').val();
+    var IdadeAcompanhantePaciente = $('#idade_acompanhante_paciente').val();
+    var LocalOcorrenciaPaciente = $('#local_ocorrencia_paciente').val();
+    var NUsbPaciente = $('#n_usab_paciente').val();
+    var NOcorrPaciente = $('#n_ocorr_paciente').val();
+    var DespPaciente = $('#desp_paciente').val();
+    var HChPaciente = $('#h_ch_paciente').val();
+    var KmFinalPaciente = $('#km_final_paciente').val();
+    var CodSiaSusPaciente = $('#cod_sia_sus_paciente').val();
+    
+    console.log(Data);
+
+     $.ajax({
+        url: 'PHP/tabela-paciente.php',
+        method: 'POST',
+        data: {
+            Data: Data,
+            NomeHospitalPaciente: NomeHospitalPaciente,
+            NomePaciente: NomePaciente,
+            IdadePaciente: IdadePaciente,
+            RgCpfPaciente: RgCpfPaciente,
+            FonePaciente: FonePaciente,
+            AcompanhantePaciente: AcompanhantePaciente,
+            IdadeAcompanhantePaciente: IdadeAcompanhantePaciente,
+            LocalOcorrenciaPaciente: LocalOcorrenciaPaciente,
+            NUsbPaciente: NUsbPaciente,
+            NOcorrPaciente: NOcorrPaciente,
+            DespPaciente: DespPaciente,
+            HChPaciente: HChPaciente,
+            KmFinalPaciente: KmFinalPaciente,
+            CodSiaSusPaciente: CodSiaSusPaciente
+        },
+        dataType: 'json'
+    }).done(function() {
+        alert("alguma coisa deu!!");
+    });
+    
+};
+
+
---
+++ PHP/tabela-paciente.php
@@ -0,0 +1,34 @@
+<?php
+include("conecta.php");
+
+$NomeHospitalPaciente = $_POST['NomeHospitalPaciente'];
+$Data = $_POST['Data'];
+$NomePaciente = $_POST['NomePaciente'];
+$IdadePaciente = $_POST['IdadePaciente'];
+$RgCpfPaciente = $_POST['RgCpfPaciente'];
+$FonePaciente = $_POST['FonePaciente'];
+$AcompanhantePaciente = $_POST['AcompanhantePaciente'];
+$IdadeAcompanhantePaciente = $_POST['IdadeAcompanhantePaciente'];
+$LocalOcorrenciaPaciente = $_POST['LocalOcorrenciaPaciente'];
+$NUsbPaciente = $_POST['NUsbPaciente'];
+$NOcorrPaciente = $_POST['NOcorrPaciente'];
+$DespPaciente = $_POST['DespPaciente'];
+$HChPaciente = $_POST['HChPaciente'];
+$KmFinalPaciente = $_POST['KmFinalPaciente'];
+$CodSiaSusPaciente = $_POST['CodSiaSusPaciente'];
+
+$stmt = $pdo->prepare("INSERT INTO paciente
+(nome_hospital_paciente, data_paciente,
+nome_paciente, idade_paciente, rg_cpf_paciente,
+fone_paciente, acompanhante_paciente, idade_acompanhante_paciente,
+local_ocorrencia_paciente, n_usb_paciente, n_ocorr_paciente,
+desp_paciente, h_ch_paciente, km_final_paciente,
+cod_sia_sus_paciente) VALUES 
+(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->execute([
+    $NomeHospitalPaciente, $Data, $NomePaciente, $IdadePaciente,
+    $RgCpfPaciente, $FonePaciente, $AcompanhantePaciente, $IdadeAcompanhantePaciente,
+    $LocalOcorrenciaPaciente, $NUsbPaciente, $NOcorrPaciente, $DespPaciente, 
+    $HChPaciente, $KmFinalPaciente, $CodSiaSusPaciente
+]);
+?>
